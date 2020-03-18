2297c3d896c632d576333c7d8c5ca75379df1b54
Roman Koyfman <koyfman.roman@gmail.com>
2020-03-18 16:09:30 -0400

    Update post.js
    Added the ability to send files.

--- post.js
+++ post.js
@@ -7,12 +7,14 @@
     ajax_url - The url of admin-ajax.php.
     _ajax_nonce - A variable used to validate requests in Wordpress.
     action - Wordpress action which this request will trigger (its actual name is "wp_ajax_$action").
-    post_data - The data.
+    post_data - The data (except for uploaded files).
     status_element - A div, p, span, or whatever where we can write messages.
     on_success(resp) - A callback for successful requests. Receives the parsed response object.
     on_error(msg) - A callback for failed requests. Receives an error message.
+    file_attachments - An object whose properties are files.
+        When submitted, they will become items in the PHP $_FILES array.
 */
-function THEELEGA_common_post(ajax_url, _ajax_nonce, action, post_data, status_element, on_success, on_error)
+function THEELEGA_common_post(ajax_url, _ajax_nonce, action, post_data, status_element, on_success, on_error, file_attachments)
 {
     var $ = jQuery;
     on_success = on_success ? on_success : function(){};
@@ -21,15 +23,17 @@ function THEELEGA_common_post(ajax_url, _ajax_nonce, action, post_data, status_e
 
     status_element.text('Please wait...');
 
-    var request = 
+    var data = new FormData();
+    data.append('_ajax_nonce', _ajax_nonce);
+    data.append('action', action);
+    data.append('post_data', JSON.stringify(post_data));
+    
+    for (var key in file_attachments)
     {
-        _ajax_nonce: _ajax_nonce,
-        action: action,
-        //Post_data is stringified to circumvent PHP's max_input_vars.
-        post_data: JSON.stringify(post_data)
+        data.append(key, file_attachments[key]);
     }
 
-    $.post(ajax_url, request).done(function onPostSuccess(response)
+    function onPostSuccess(response)
     {
         if (!(response.trim()))
         {
@@ -56,10 +60,21 @@ function THEELEGA_common_post(ajax_url, _ajax_nonce, action, post_data, status_e
             status_element.text('Error: ' + response);
             on_error(resp_obj);
         }
-    })
-    .error(function onPostError(xhr, status, error)
+    }
+    
+    function onPostError(xhr, status, error)
     {
         status_element.text('Error: ' + error);
         on_error('Error: '  + error);
+    }
+
+    jQuery.ajax(ajax_url,
+    {
+        processData: false,
+        contentType: false,
+        method:'POST',
+        success: onPostSuccess,
+        error: onPostError,
+        data: data
     });
 }
